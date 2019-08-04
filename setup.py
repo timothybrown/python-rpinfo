526193bea235ddad5df793758a8eb45beec3431a
Timothy <sysop@timb.us>
2019-08-04 07:51:52 +0000

    Fixed URL in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Gathers platform information of the currently running Raspberry Pi.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/timothybrown/rpinfo",
+    url="https://github.com/timothybrown/python-rpinfo",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
